3d03a3521d4905c921fa47df72c3d15e9dd986de
Rachael Andrew <r.andrew.dev@gmail.com>
2020-08-01 21:54:58 -0500

    Updates

---
+++ precache-manifest.e253c4ead443bb4724ccdd4ca7547c54.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55eed2525139ff464c8c4185397fa2e",
+    "url": "/jungle-jumanji/index.html"
+  },
+  {
+    "revision": "0ebc2abf59b421a791af",
+    "url": "/jungle-jumanji/static/css/main.691ae869.chunk.css"
+  },
+  {
+    "revision": "c9212f9bf7f8a2751ec4",
+    "url": "/jungle-jumanji/static/js/2.b2e125eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jungle-jumanji/static/js/2.b2e125eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ebc2abf59b421a791af",
+    "url": "/jungle-jumanji/static/js/main.16461a98.chunk.js"
+  },
+  {
+    "revision": "d27a465fd2f153a3ef6b",
+    "url": "/jungle-jumanji/static/js/runtime-main.5e09ed50.js"
+  },
+  {
+    "revision": "302d2317c70935055ee7de38c8f18ab5",
+    "url": "/jungle-jumanji/static/media/antelope-27.302d2317.png"
+  },
+  {
+    "revision": "3af0bc2d343e5d8e7b5b8edae1a3c3e7",
+    "url": "/jungle-jumanji/static/media/background.3af0bc2d.jpg"
+  },
+  {
+    "revision": "9d3db8f9844e3e77e730f7fe25a9675e",
+    "url": "/jungle-jumanji/static/media/baloo-29.9d3db8f9.jpg"
+  },
+  {
+    "revision": "61add5adbdf6392e5f3bed05a76331c3",
+    "url": "/jungle-jumanji/static/media/bears-19.61add5ad.jpg"
+  },
+  {
+    "revision": "d1bc2a991f886b1dc21a1d37c3020bfd",
+    "url": "/jungle-jumanji/static/media/cage-17.d1bc2a99.jpg"
+  },
+  {
+    "revision": "c03cfc9949a89b248e0e1088ef90d98d",
+    "url": "/jungle-jumanji/static/media/cobras-23.c03cfc99.jpg"
+  },
+  {
+    "revision": "85a3f468c734642b942bc087c9d386e9",
+    "url": "/jungle-jumanji/static/media/cobras-5.85a3f468.jpg"
+  },
+  {
+    "revision": "9a4c0fcd5788a52925693778f985caa1",
+    "url": "/jungle-jumanji/static/media/cows-22.9a4c0fcd.jpg"
+  },
+  {
+    "revision": "9db426ab42babb7002242244aaf4420c",
+    "url": "/jungle-jumanji/static/media/eyes-6.9db426ab.jpg"
+  },
+  {
+    "revision": "a6dce16c08a5bd3c9dcdce349abb42bd",
+    "url": "/jungle-jumanji/static/media/fire-28.a6dce16c.jpg"
+  },
+  {
+    "revision": "f7ff8fb4c2ceaf864a1819cafdedd575",
+    "url": "/jungle-jumanji/static/media/flea-14.f7ff8fb4.jpg"
+  },
+  {
+    "revision": "a6b570affcf6acbf496ae1a0a9a28782",
+    "url": "/jungle-jumanji/static/media/hypnotize-11.a6b570af.jpg"
+  },
+  {
+    "revision": "3abc477e866f4a85c3d635a8d33d1129",
+    "url": "/jungle-jumanji/static/media/jungle-4.3abc477e.jpg"
+  },
+  {
+    "revision": "a7c551774656efa5e17eae44526bf9c9",
+    "url": "/jungle-jumanji/static/media/monkeys-1.a7c55177.jpg"
+  },
+  {
+    "revision": "aad25e7921aa7707909f42b614c28c24",
+    "url": "/jungle-jumanji/static/media/monkeys-24.aad25e79.jpg"
+  },
+  {
+    "revision": "011d0f6068ddadeda14a04b8f753004a",
+    "url": "/jungle-jumanji/static/media/monkeys-25.011d0f60.jpg"
+  },
+  {
+    "revision": "83db8254e1795a2068f8d3921e40861c",
+    "url": "/jungle-jumanji/static/media/monkeys-26.83db8254.jpg"
+  },
+  {
+    "revision": "aa15a1334a5eeb08465a23edba9ab24d",
+    "url": "/jungle-jumanji/static/media/nojungle-13.aa15a133.png"
+  },
+  {
+    "revision": "4806c6654ea320106694e3f6be80adb5",
+    "url": "/jungle-jumanji/static/media/python-2.4806c665.jpg"
+  },
+  {
+    "revision": "5d8d9c2b31a047a5540878e0869e744c",
+    "url": "/jungle-jumanji/static/media/python-3.5d8d9c2b.jpg"
+  },
+  {
+    "revision": "5d45fc32fe252d963d4ba847867639c3",
+    "url": "/jungle-jumanji/static/media/rest-10.5d45fc32.jpg"
+  },
+  {
+    "revision": "5d45fc32fe252d963d4ba847867639c3",
+    "url": "/jungle-jumanji/static/media/rest-8.5d45fc32.jpg"
+  },
+  {
+    "revision": "5d45fc32fe252d963d4ba847867639c3",
+    "url": "/jungle-jumanji/static/media/rest-9.5d45fc32.jpg"
+  },
+  {
+    "revision": "80d62f3139a858adbe3920cd68e0bb5e",
+    "url": "/jungle-jumanji/static/media/signpost-18.80d62f31.jpg"
+  },
+  {
+    "revision": "0e7617e8c4506c1fb19e27f67b6acbda",
+    "url": "/jungle-jumanji/static/media/snakebite-15.0e7617e8.jpg"
+  },
+  {
+    "revision": "e5062390c931c3babd996b1876046f46",
+    "url": "/jungle-jumanji/static/media/thorns-20.e5062390.jpg"
+  },
+  {
+    "revision": "de4a2b596f6c1900a0186b5f007e1d98",
+    "url": "/jungle-jumanji/static/media/tiger-21.de4a2b59.jpg"
+  },
+  {
+    "revision": "055fedfb71b52c2326ecdca3a1131ad6",
+    "url": "/jungle-jumanji/static/media/tiger-roaring-30.055fedfb.jpg"
+  },
+  {
+    "revision": "855a9549f7a5c9309762f41a40ffbd5c",
+    "url": "/jungle-jumanji/static/media/vultures-12.855a9549.jpg"
+  },
+  {
+    "revision": "93965216dbe7a4cb5c8edacf5c23453b",
+    "url": "/jungle-jumanji/static/media/werewolf-pack-16.93965216.jpg"
+  },
+  {
+    "revision": "722edb42bfda1a0891c9017afad50699",
+    "url": "/jungle-jumanji/static/media/wolves-7.722edb42.jpg"
+  }
+]);
